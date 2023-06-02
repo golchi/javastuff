40fac2016e97e14f89c2d5dbfe9322a5a2e63dd6
Mawusee FOLI-AWLI <mawusee.foli@gmail.com>
2023-06-01 21:23:44 -0400

    Chap3 - MassAndWeight class added

--- src/main/java/chap3/MassAndWeight.java
+++ src/main/java/chap3/MassAndWeight.java
@@ -1,4 +1,20 @@
 package chap3;
 
+import javax.swing.*;
+
 public class MassAndWeight {
+    public static void main(String[] args){
+        double mass, weight;
+
+        mass = Double.parseDouble(JOptionPane.showInputDialog("Enter your mass (kg): "));
+        weight = Math.round(mass * 9.8);
+
+        if (weight > 1000) {
+            JOptionPane.showMessageDialog(null," Weight: " +
+                    weight +" -  Your object is too heavy");
+        } else if(weight < 10 ){
+            JOptionPane.showMessageDialog(null," Weight: " +
+                    weight +" -  You object is too light");
+        }
+    }
 }
